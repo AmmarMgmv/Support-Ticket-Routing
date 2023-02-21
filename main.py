a13e45e3238d4c08216f7db4c9556d08466cd77d
ClaireOG <123943797+ClaireOG@users.noreply.github.com>
2023-02-21 00:07:04 +0000

    Update main.py
    changed layout so that two dash tables could be displayed

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-
-from dash import Dash, dash_table, dcc, html
-from dash.dependencies import Input, Output
+import numpy as np
 import pandas as pd
+from itertools import product
+import dash
+from dash import html
+from dash import dcc
+from dash import dash_table
+import dash_table_experiments as dt
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 tagDataset = pd.read_csv("Dataset\Tags.csv", encoding = "ISO-8859-1")
 qDataset = pd.read_csv("Dataset\Questions.csv", encoding = "ISO-8859-1")
 aDataset = pd.read_csv("Dataset\Answers.csv", encoding = "ISO-8859-1")
+
+sortedTags = tagDataset.groupby(["Tag"]).size() #count how many in each "group: "Tag" 
+sortByTop = sortedTags.sort_values(ascending=False) #sort to show top categories
+df_tag = pd.DataFrame({'Tag': sortByTop.index, 'Occurrences': sortByTop.values})
+df_tag.head(15)
 
 TagsAnswers = pd.merge(tagDataset, aDataset, left_on='Id', right_on='ParentId')
 summedScore = TagsAnswers.groupby(['Tag', 'OwnerUserId'])['Score'].sum().reset_index()
@@ -14,28 +25,54 @@ topScoreOwners = summedScore.iloc[topScoreOwners['Score']]
 sortedTopScoreOwners = topScoreOwners.sort_values('Score', ascending=False)
 df=sortedTopScoreOwners
 
-#table that shows top userID by tag and their score, also allows to user to search
-#------------------------------------------------------------------------------
-app = Dash(__name__)
 
-app.layout = dash_table.DataTable(
-    df.to_dict('records'),
-     columns=[
-        {'name': 'Issue', 'id': 'Tag'},
-        {'name': 'ID no. of staff member', 'id': 'OwnerUserId'},
-        {'name': 'Score', 'id': 'Score'},
-    ],
-    filter_action='native',
 
-    style_table={
-        'height': 15,
-    },
-    style_data={
-        'width': '180 px', 'minWidth': '180 px', 'maxWidth': '180 px',
-        'overflow': 'hidden',
-        'textOverflow': 'ellipsis',
-    },
-    )
+app = dash.Dash()
+
+
+
+filters = html.Div(children=[
+        html.Div(dash_table.DataTable(
+            df_tag.to_dict('records'),
+            columns=[
+                {'name': 'Issue', 'id': 'Tag'},
+                {'name': 'Amount', 'id': 'Occurrences'},
+            ],
+            filter_action='native',
+            page_size=20,
+            fixed_rows={'headers': True},
+            style_header={
+                'backgroundColor': 'rgb(30, 30, 30)',
+                'color': 'white',
+            },
+            style_data={
+                'overflow': 'hidden',
+                'textOverflow': 'ellipsis',
+            },
+    )),
+    html.Div(dash_table.DataTable(
+        df.to_dict('records'),
+        columns=[
+            {'name': 'Issue', 'id': 'Tag'},
+            {'name': 'ID no. of staff member', 'id': 'OwnerUserId'},
+            {'name': 'Score', 'id': 'Score'},
+        ],
+        filter_action='native',
+        page_size=20,
+        fixed_rows={'headers': True},
+        style_header={
+                'backgroundColor': 'rgb(30, 30, 30)',
+                'color': 'white',
+            },
+        style_data={
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        ))
+])
+
+
+app.layout = html.Div(children=[filters])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
