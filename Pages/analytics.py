bca52cfabb25d0e8f6801e84f7b09e7e39daa1ac
ClaireOG <123943797+ClaireOG@users.noreply.github.com>
2023-03-21 12:15:30 +0000

    Update analytics.py
    Added code from dash branch that we want to display on analytics page

--- Pages/analytics.py
+++ Pages/analytics.py
@@ -4,8 +4,72 @@ import plotly.express as px
 
 dash.register_page(__name__)
 
-layout = html.Div(
-    [
-    dcc.Markdown('# This will be the content of the analytics page')
-    ]
-)
+layout = html.Div(children=[
+    html.H1(children='Graph showing the top 20 most searched tags',
+            style={
+                'textAlign': 'center',
+                'font-family':'Arial'}),
+    dcc.Graph(
+        id='tag-graph',
+        figure=fig
+    ),
+    html.H1(children='Table showing tags and frequency',
+            style={
+                'textAlign': 'center',
+                'font-family':'Arial'}),
+    html.Div(dash_table.DataTable(
+        tagsDF.to_dict('records'),
+        columns=[
+            {'name': 'Tag', 'id': 'Tag'},
+            {'name': 'Frequency', 'id': 'Occurrences'},
+        ],
+        filter_action='native',
+        page_size=20,
+        fixed_rows={'headers': True},
+        style_header={
+            'backgroundColor': 'rgb(30, 30, 30)',
+            'color': 'white',
+        },
+        style_data={
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+    )),
+    html.H1(children='Graph showing tags searched by month and year',
+            style={
+                    'textAlign': 'center',
+                   'font-family':'Arial'}),
+    dcc.Graph(
+        id='MY-graph',
+        figure=fig2
+    ),
+    html.H1(children='Graph showing the number of questions per day',style={'textAlign': 'center','font-family':'Arial'}),
+    dcc.Graph(
+        id='questions-graph',
+        figure=fig3
+    ),
+    html.H1(children='Table showing top ID for each tag',
+            style={
+                'textAlign': 'center',
+                'font-family' : 'Arial'
+            }),
+    html.Div(dash_table.DataTable(
+        sortedTopScoreOwners.to_dict('records'),
+        columns=[
+            {'name': 'Issue', 'id': 'Tag'},
+            {'name': 'ID no. of staff member', 'id': 'OwnerUserId'},
+            {'name': 'Score', 'id': 'Score'},
+        ],
+        filter_action='native',
+        page_size=20,
+        fixed_rows={'headers': True},
+        style_header={
+                'backgroundColor': 'rgb(30, 30, 30)',
+                'color': 'white',
+            },
+        style_data={
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        ))
+])
