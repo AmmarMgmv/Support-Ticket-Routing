ed5d7a437b34a4412f67f6b883e9e741e2eb57a9
WeijianWWWW <123949187+WeijianWWWW@users.noreply.github.com>
2023-03-30 15:36:26 +0100

    Update search.py

--- Pages/search.py
+++ Pages/search.py
@@ -1,21 +1,56 @@
 import dash
 from dash import html, dcc 
 import plotly.express as px
+from dash.dependencies import Input, Output, State
 from apps import navigation
+from main import *
 
-search_layout = html.Div(
+def testfunc(input):
+    if input != "":
+        question = "This is the " + input + " question"
+        answer = "This is the " + input + " answer"
+        return question, answer
+
+search_layout = html.Div(children=
     [
         navigation.navbar,
-        html.Br(),
-        html.Br(),
-        html.Div("welcome to search"),
-    ]
+        html.Div(
+            className="searchContainer",
+            children=[
+                html.Div(
+                    # action="{{ url_for('search') }}",
+                    # method="POST",
+                    className="search-bar",
+                    children=[
+                        dcc.Input(
+                            placeholder="Search through previously asked questions...",
+                            type="text",
+                            value="",
+                            # name="search-input",
+                            id="userInput"
+                        ),
+                        html.Button(
+                            type="Search",
+                            children=[
+                                html.Img(
+                                    src="https://img.icons8.com/ios/256/search.png"
+                                )
+                            ],
+                            n_clicks=0,
+                            id="mybutton"
+                        )
+                    ]
+                ),
+                html.Div(
+                    className="searchForm",
+                    children=[
+                        html.Div(
+                            id='presults',
+                            style={'overflow': 'auto', 'height': '100%', 'padding':'2rem'}
+                        ),
+                    ],
+                )
+            ]
+        ), 
+    ],
 )
-
-# dash.register_page(__name__)
-
-# layout = html.Div(
-#     [
-#         dcc.Markdown('# This will be the content of the search page')
-#     ]
-# )
