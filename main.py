6a4e83d4887ffe6c144e9a8fb2dadec10d147db7
udit10jain <udit123jain@gmail.com>
2023-03-30 16:06:57 +0100

    updated fuzzy search

--- main.py
+++ main.py
@@ -67,11 +67,31 @@ groups = TagsQs.groupby('Tag')
 # 
 # ------------------------------------------------------------------------------------------
 
+# import the package to work on the datasets
+import pandas as pd
+import spacy
+from lxml.html import fromstring
+import re
+
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
-    
+
+#load the datasets 
+# specify the columns you want to read
+columns_to_read = ['Body', 'Id']
+quesDataset = pd.read_csv("Dataset\Questions.csv", encoding = "ISO-8859-1",usecols=columns_to_read)
+
+columns_to_read_answers = ['Body', 'ParentId']
+ansDataset = pd.read_csv("Dataset\Answers.csv",encoding = "ISO-8859-1",usecols=columns_to_read_answers)
+ansDataset = ansDataset.rename(columns={'Body': 'Answer_Body'})
+
+merged_df = pd.merge(quesDataset, ansDataset, left_on='Id', right_on='ParentId')
+
+#print("success")
+
 # fuzzy search on question data set
+
 from whoosh.fields import Schema, TEXT, ID
 from whoosh import index, qparser
 from whoosh.analysis import RegexTokenizer, LowercaseFilter
@@ -82,7 +102,7 @@ import os
 my_analyzer = RegexTokenizer() | LowercaseFilter()
 
 # Define the schema for the index
-schema = Schema(Body=TEXT(stored=True, analyzer=my_analyzer))
+schema = Schema(Body=TEXT(stored=True, analyzer=my_analyzer),Answer_Body=TEXT(stored=True, analyzer=my_analyzer))
 
 # Create the index directory if it doesn't exist
 if not os.path.exists("index_dir"):
@@ -92,13 +112,15 @@ if not os.path.exists("index_dir"):
 ix = index.create_in("index_dir", schema)
 writer= ix.writer()
 
-for i, row in qDataset.iterrows():
+for i, row in merged_df.iterrows():
     Body = remove_html_tags (row ["Body"])
-    ##answerBody = remove_html_tags(row["answerBody"])
-    writer.add_document(Body=Body)
+    Answer_Body = remove_html_tags(row["Answer_Body"])
+    writer.add_document(Body=Body,Answer_Body=Answer_Body)
     if i == 10000:
         break
 writer.commit()
+
+
 
 # Function to search the index
 def index_search(dirname, search_fields, search_query):
@@ -111,27 +133,31 @@ def index_search(dirname, search_fields, search_query):
     q = mp.parse(search_query + "~")
     
     with ix.searcher() as searcher:
-        results = searcher.search(q, limit=5)
+        results = searcher.search(q, limit=None)
         
-        # Build a dictionary of search results
-        results_dict = {}
+        # Build a list of search results
+        results_list = []
         for hit in results:
-            results_dict[hit.fields()["Body"]] = hit.score
+            result_dict = {}
+            result_dict["QuestionBody"] = hit.fields()["Body"]
+            result_dict["AnswerBody"] = hit.fields()["Answer_Body"]
+            results_list.append(result_dict)
             
-        return results_dict
+        return results_list
 
 
 #Prompt the user for search queries
-while True:
-    query = input("Enter your query (or 0 to exit): ")
-    if query == "0":
-        break
-    else:
-        results = index_search("index_dir", ["Body"], query)
-        print("Search results:")
-        for i, result in enumerate(results, start=1):
-         print(f"{i}. {result}\n")
-
+# while True:
+#     query = input("Enter your query (or 0 to exit): ")
+#     if query == "0":
+#         break
+#     else:
+#         results = index_search("index_dir", ["Body"], query)
+#         print("Search results:")
+#         for i, result in enumerate(results, start=1):
+#             print(f"{i}. Question: {result['QuestionBody']}")
+#             print(f"   Answer: {result['AnswerBody']}")
+#             print("")
 # ------------------------------------------------------------------------------------------
 # 
 #                            FIND OVERLAPPING WORDS & DETECT TAGS                               
