76a31a729e78b803263177781c8649a003a9f730
ClaireOG <123943797+ClaireOG@users.noreply.github.com>
2023-04-02 00:35:49 +0100

    Update analytics.py
    Added piechart with dropdown

--- Pages/analytics.py
+++ Pages/analytics.py
@@ -13,6 +13,10 @@ sortedTopScoreOwners = dataReader.topOwnerIdTag(main.tagDataset, main.aDataset)
 fig = px.bar(tagsDF.head(20), x='Tag', y='Occurrences', barmode="group")                 # barchart showing most searched tags
 fig2 = px.scatter(QsPerMonthYear, x='Date', y='Count')                                   # scatter graph showing year-month and no. of questions
 fig3 = px.bar(QsPerDay, x='Date', y='QsAsked', barmode="group")                          # barchart showing date and no. of questions
+pieC = px.pie(AllQsQueries, values=None, names='Tag', 
+             title='Unanswered and Answered Questions per Tag', 
+             hover_data={'QuestionsAsked':True, 'QuestionsUnans':True, 'QuestionsAns':True},
+             labels={'QuestionsUnans':'Unanswered', 'QuestionsAns':'Answered'})
 
 analytics_layout = html.Div(children=[
     navigation.navbar,
@@ -54,6 +58,12 @@ analytics_layout = html.Div(children=[
         id='MY-graph',
         figure=fig2
     ),
+    dcc.Dropdown(
+        id="dropdown",
+        options=[{"label": tag, "value": tag} for tag in AllQsQueries["Tag"].unique()],
+        value=AllQsQueries["Tag"].unique()[0]
+    ),
+    dcc.Graph(id="pie-chart"),
     html.H1(children='Graph showing the number of questions per day',
             style={
                 'textAlign': 'center',
@@ -87,3 +97,29 @@ analytics_layout = html.Div(children=[
         },
         ))
 ])
+
+@app.callback(
+    dash.dependencies.Output("pie-chart", "figure"),
+    [dash.dependencies.Input("dropdown", "value")]
+)
+def update_pie_chart(tag):
+    # Filter the AllQsQueries DataFrame by the selected tag
+    data = AllQsQueries[AllQsQueries["Tag"] == tag]
+
+    # Check if there are any unanswered or answered questions for this tag
+    if data["QuestionsAns"].iloc[0] == 0 and data["QuestionsUnans"].iloc[0] == 0:
+        pieC = px.pie(names=["No Data"], values=[1])
+        pieC.update_layout(title=f"No Answered or Unanswered Questions for {tag} Tag")
+    else:
+        values = []
+        names = []
+        if data["QuestionsAns"].iloc[0] > 0:
+            values.append(data["QuestionsAns"].iloc[0])
+            names.append("Answered")
+        if data["QuestionsUnans"].iloc[0] > 0:
+            values.append(data["QuestionsUnans"].iloc[0])
+            names.append("Unanswered")
+        pieC = px.pie(values=values, names=names, hole=.3)
+        pieC.update_layout(title=f"Answered and Unanswered Questions for {tag} Tag")
+
+    return pieC
